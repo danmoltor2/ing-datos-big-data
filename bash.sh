46f21973e5b60ea4a8077a146f6a136dfe542ca5
José María Luna <josemarialunaromera@gmail.com>
2025-03-10 16:52:37 +0100

    checkpoint

--- bash.sh
+++
@@ -1,1 +0,0 @@
-docker exec -it hiveserver2 beeline -u jdbc:hive2://hiveserver2:10000
--- test.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO ventas VALUES (1, 'Laptop', 1200.50), (2, 'Mouse', 25.75);
-
-SELECT * FROM ventas;
-DESCRIBE ventas;
-DESCRIBE FORMATTED nombre_de_la_tabla;
